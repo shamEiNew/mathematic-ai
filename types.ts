5652371955ce5c6029de17a945e7b19e9501b223
Sham S <panchalshamsundar7@gmail.com>
2025-11-07 17:54:44 +0530

    removed all typescript files

--- types.ts
+++
@@ -1,9 +0,0 @@
-// FIX: The `content` property is now an HTML string, not a ReactNode, so the type is changed to `string`. The `React` import is no longer needed.
-export interface BlogPost {
-  slug: string;
-  title: string;
-  date: string;
-  author: string;
-  summary: string;
-  content: string;
-}
